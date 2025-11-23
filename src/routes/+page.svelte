08cae07ddaff220318ae7451a598b7224c1ca30a
vradient <167614599+TacoEnjoyer@users.noreply.github.com>
2025-11-23 18:56:06 +0700

    rework

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="font-sans font-bold text-center pt-50 text-6xl">Everything will going to be reworked</h1>
+<p class="font-sans font-semibold text-center pt-20 text-4xl">In short. I'm Vradient :)</p>
